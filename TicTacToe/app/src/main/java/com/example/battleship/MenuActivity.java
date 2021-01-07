e2919771523001070153750e569c859264b39dd3
saba024 <naskidashvili.2001@yahoo.com>
2021-01-08 00:20:07 +0300

    lab3

---
+++ TicTacToe/app/src/main/java/com/example/battleship/MenuActivity.java
@@ -0,0 +1,32 @@
+package com.example.battleship;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void EndGame(View view) {
+        int pid = android.os.Process.myPid();
+        android.os.Process.killProcess(pid);
+    }
+
+    public void ShowAboutNote(View view) {
+        Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
+        startActivity(intent);
+    }
+
+    public void StartGameOnline(View view) {
+        Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+    }
+}
+
---
+++ TicTacToe/app/src/main/java/com/example/battleship/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.battleship;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Thread timer= new Thread()
+        {
+            public void run()
+            {
+                try
+                {
+                    sleep(3000);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}
